869a35094388e511330be17b5b71001c3d7ad158
Shreya Sundriyal <88231708+shreyeahh@users.noreply.github.com>
2021-08-08 20:34:31 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about MyComponents 
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente quae blanditiis vitae officiis excepturi, enim, laboriosam optio rerum hic quas, velit laborum esse alias cupiditate nesciunt rem voluptas modi quia?</p>
+        </div>
+    )
+}
